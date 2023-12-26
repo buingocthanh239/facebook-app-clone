c6fc82575ca57a753db8a8ae485a0e5dcb9639e8
buingocthanh239 <buingocthanh2392002@gmail.com>
2023-12-26 15:08:30 +0700

    fix: fix  bug re call api in use Loading list hook

--- src/hooks/useLoadingListApi.tsx
+++ src/hooks/useLoadingListApi.tsx
@@ -1,11 +1,16 @@
 import { useCallback, useEffect, useState } from 'react';
 import { COUNT_ITEM } from 'src/common/constants';
 import { IListBodyResponse } from 'src/interfaces/common.interface';
+import { useAppDispatch } from 'src/redux';
+import { setMessage } from 'src/redux/slices/appSlice';
+import { handShowErrorMessage } from 'src/utils/helper';
 
 function useLoadingListApi<T>(
   // eslint-disable-next-line no-unused-vars
-  callbackFunc: (data: { index: number; count: number }) => Promise<IListBodyResponse<T>>
+  callbackFunc: (data: any) => Promise<IListBodyResponse<T>>,
+  arg?: object
 ) {
+  const dispatch = useAppDispatch();
   const [data, setData] = useState<T[]>([]);
   const [isFetch, setIsFetch] = useState<boolean>(true);
   const [isLoadingFirstApi, setIsLoadingFirstAPi] = useState<boolean>(false);
@@ -17,21 +22,23 @@ function useLoadingListApi<T>(
     async function getFirstUsers() {
       try {
         setIsLoadingFirstAPi(true);
-        const res = await callbackFunc({ index: 0, count: COUNT_ITEM });
+        const res = await callbackFunc({ ...arg, index: 0, count: COUNT_ITEM });
         if (res.success) {
           setData(res.data);
           if (res.data.length < COUNT_ITEM) {
             setIsFetch(false);
           }
+        } else {
+          dispatch(setMessage(handShowErrorMessage(parseInt(res.code as unknown as string))));
         }
       } catch (err) {
-        console.log(err);
+        dispatch(setMessage('Vui lòng kiểm tra lại kết nối'));
       } finally {
         setIsLoadingFirstAPi(false);
       }
     }
     getFirstUsers();
-  }, [callbackFunc]);
+  }, [arg, callbackFunc, dispatch]);
 
   useEffect(() => {
     getListUsers();
@@ -46,18 +53,20 @@ function useLoadingListApi<T>(
   const getRefreshingApi = useCallback(() => {
     async function getRefreshing() {
       try {
-        const res = await callbackFunc({ index: skip, count: COUNT_ITEM });
+        const res = await callbackFunc({ ...arg, index: skip, count: COUNT_ITEM });
         if (res.success) {
           setData(res.data);
+        } else {
+          dispatch(setMessage(handShowErrorMessage(parseInt(res.code as unknown as string))));
         }
       } catch (err) {
-        console.log(err);
+        dispatch(setMessage('Vui lòng kiểm tra lại kết nối'));
       } finally {
         setRefreshing(false);
       }
     }
     getRefreshing();
-  }, [callbackFunc, skip]);
+  }, [arg, callbackFunc, dispatch, skip]);
 
   // gọi api khi refeshing
   useEffect(() => {
@@ -70,16 +79,18 @@ function useLoadingListApi<T>(
     if (isFetch) {
       try {
         setNextFetchingAPi(true);
-        const res = await callbackFunc({ index: skip + COUNT_ITEM, count: COUNT_ITEM });
+        const res = await callbackFunc({ ...arg, index: skip + COUNT_ITEM, count: COUNT_ITEM });
         if (res.success) {
           if (res.data.length === 0) {
             return setIsFetch(false);
           }
-          setData(blockUsers => [...blockUsers, ...res.data]);
           setSkip(skip => skip + COUNT_ITEM);
+          setData(data => [...data, ...res.data]);
+        } else {
+          dispatch(setMessage(handShowErrorMessage(parseInt(res.code as unknown as string))));
         }
       } catch (err) {
-        console.log(err);
+        dispatch(setMessage('Vui lòng kiểm tra lại kết nối'));
       } finally {
         setNextFetchingAPi(false);
       }
