cb1729c124c29d41d607c22d9aa9e4b73ffad7a2
NHT18102002 <truyen18102002@gmail.com>
2024-01-04 13:06:34 +0700

    feat: api-get-mark-comment

--- src/components/Post/components/Comment/CommentModal.tsx
+++ src/components/Post/components/Comment/CommentModal.tsx
@@ -1,149 +1,365 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableHighlight } from 'react-native';
 import styles from './styles';
 import Modal from 'react-native-modal';
-import PostReactions from './PostReaction/PostReactions';
 import { color } from 'src/common/constants/color';
 import { ScrollView } from 'react-native';
-import ListComments from './ListComment/ListComment';
+import BaseFlatList from 'src/components/BaseFlatList';
+import { ActivityIndicator, Avatar } from 'react-native-paper';
+import { getAvatarUri } from 'src/utils/helper';
+import { useEffect, useState } from 'react';
+import { IGetListFeels, IGetMarkComment, IListFeels } from 'src/interfaces/comments.interface';
+import { getListFeelsApi, getMarkCommentApi } from 'src/services/comment.service';
+import { IListCommentPost } from 'src/interfaces/comments.interface';
+import { coverTimeToNow } from 'src/utils/dayjs';
+import AntdIcon from 'react-native-vector-icons/AntDesign';
 interface CommentTabProps {
+  id: string;
   openModal: boolean;
   setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 const CommentTab = (props: CommentTabProps) => {
-  const Emotions = [
-    {
-      id: 1,
-      name: 'Like',
-      icon: 'like2',
-      library: 'AntDesign',
-      number: 100,
-      color: `${color.likeIcon}`
-    },
-    {
-      id: 2,
-      name: 'Love',
-      icon: 'heart',
-      library: 'FontAwesome6',
-      number: 80,
-      color: `${color.heartIcon}`
-    },
-    {
-      id: 3,
-      name: 'Haha',
-      icon: 'laugh',
-      library: 'Ionicons',
-      number: 60,
-      color: `${color.laughtIcon}`
-    },
-    {
-      id: 4,
-      name: 'Sad',
-      icon: 'face-sad-cry',
-      library: 'FontAwesome6',
-      number: 120,
-      color: `${color.likeIcon}`
-    },
-    {
-      id: 5,
-      name: 'Angry',
-      icon: 'face-angry',
-      library: 'FontAwesome6',
-      number: 20,
-      color: `${color.likeIcon}`
-    }
-  ];
+  const { id, openModal, setOpenModal } = props;
+  const [listMarkComment, setListMarkComment] = useState<IListCommentPost[]>([]);
+  const [skip, setSkip] = useState<number>(0);
+  const [isNext, setIsNext] = useState<boolean>(false);
+  const [isNextFetch, setIsNextFetch] = useState<boolean>(true);
+  const [isLoadingFirstApi, setIsLoadingFirstAPi] = useState<boolean>(false);
+  const [listFeels, setListFeels] = useState<IListFeels[]>([]);
+  const [totalFeel, setTotalFeel] = useState(0);
+  const COUNT_ITEM = 10;
+  useEffect(() => {
+    const data: IGetMarkComment = {
+      id: id,
+      index: 0,
+      count: 10
+    };
+    const fetchData = async (data: IGetMarkComment) => {
+      setIsLoadingFirstAPi(true);
+      try {
+        const result = await getMarkCommentApi(data);
+        if (result.success) {
+          if (!result.data.length) {
+            setIsNextFetch(false);
+            return;
+          }
+          setListMarkComment(result.data);
+          setIsNextFetch(true);
+          setSkip(COUNT_ITEM);
+          // console.log('result',result)
+        }
+        setTimeout(() => {
+          setIsLoadingFirstAPi(false);
+        }, 200);
+      } catch (error) {
+        return console.log({ message: 'sever availability' });
+      }
+    };
+    fetchData(data).catch(console.error);
+  }, [id]);
 
-  const listComments = [
-    {
-      id: 1,
-      author: 'Truyền',
-      content: 'Đây là comment.',
-      avatarUrl:
-        'https://media-cdn-v2.laodong.vn/storage/newsportal/2023/7/25/1220914/Rose.jpg?w=660',
-      timePost: '1 Giờ'
-    },
-    {
-      id: 2,
-      author: 'Truyền',
-      content: 'Another comment here.',
-      avatarUrl:
-        'https://media-cdn-v2.laodong.vn/storage/newsportal/2023/7/25/1220914/Rose.jpg?w=660',
-      timePost: '1 ngày'
-    },
-    {
-      id: 3,
-      author: 'Truyền',
-      content: 'Another comment here.',
-      avatarUrl:
-        'https://media-cdn-v2.laodong.vn/storage/newsportal/2023/7/25/1220914/Rose.jpg?w=660',
-      timePost: '2 ngày'
-    },
-    {
-      id: 4,
-      author: 'Truyền',
-      content: 'Another comment here.',
-      avatarUrl:
-        'https://media-cdn-v2.laodong.vn/storage/newsportal/2023/7/25/1220914/Rose.jpg?w=660',
-      timePost: '1 ngày'
-    },
-    {
-      id: 5,
-      author: 'Truyền',
-      content: 'Another comment here.',
-      avatarUrl:
-        'https://media-cdn-v2.laodong.vn/storage/newsportal/2023/7/25/1220914/Rose.jpg?w=660',
-      timePost: '5 phút'
-    },
-    {
-      id: 6,
-      author: 'Truyền',
-      content: 'Another comment here.',
-      avatarUrl:
-        'https://media-cdn-v2.laodong.vn/storage/newsportal/2023/7/25/1220914/Rose.jpg?w=660',
-      timePost: '1 ngày'
-    },
-    {
-      id: 7,
-      author: 'Truyền',
-      content: 'Another comment here.',
-      avatarUrl:
-        'https://media-cdn-v2.laodong.vn/storage/newsportal/2023/7/25/1220914/Rose.jpg?w=660',
-      timePost: '1 ngày'
-    }
-    // ...Thêm các comment khác vào đây
-  ];
+  useEffect(() => {
+    const data: IGetListFeels = {
+      id: id,
+      index: 0,
+      count: 100
+    };
+    const fetchData = async (data: IGetListFeels) => {
+      try {
+        const result = await getListFeelsApi(data);
+        if (result.success) {
+          if (!result.data.length) {
+            return;
+          } else setTotalFeel(result.data.length);
+          setListFeels(result.data);
+        }
+      } catch (error) {
+        return console.log({ message: 'sever availability' });
+      }
+    };
+    fetchData(data).catch(console.error);
+  }, [id]);
+  // const Emotions = [
+  //   {
+  //     id: 1,
+  //     name: 'Like',
+  //     icon: 'like2',
+  //     library: 'AntDesign',
+  //     number: 100,
+  //     color: `${color.likeIcon}`
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Love',
+  //     icon: 'heart',
+  //     library: 'FontAwesome6',
+  //     number: 80,
+  //     color: `${color.heartIcon}`
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Haha',
+  //     icon: 'laugh',
+  //     library: 'Ionicons',
+  //     number: 60,
+  //     color: `${color.laughtIcon}`
+  //   },
+  //   {
+  //     id: 4,
+  //     name: 'Sad',
+  //     icon: 'face-sad-cry',
+  //     library: 'FontAwesome6',
+  //     number: 120,
+  //     color: `${color.likeIcon}`
+  //   },
+  //   {
+  //     id: 5,
+  //     name: 'Angry',
+  //     icon: 'face-angry',
+  //     library: 'FontAwesome6',
+  //     number: 20,
+  //     color: `${color.likeIcon}`
+  //   }
+  // ];
 
+  // const listComments = [
+  //   {
+  //     "id": "261",
+  //     "mark_content": "so good",
+  //     "type_of_mark": "0",
+  //     "created": "2023-12-24T18:10:58.228Z",
+  //     "poster": {
+  //       "id": "529",
+  //       "name": "username",
+  //       "avatar": "https://it4788.catan.io.vn/files/avatar-1702202794904-871163289.png"
+  //     },
+  //     "comments": []
+  //   },
+  //   {
+  //     "id": "133",
+  //     "mark_content": "so good",
+  //     "type_of_mark": "1",
+  //     "created": "2023-12-19T12:33:34.999Z",
+  //     "poster": {
+  //       "id": "841",
+  //       "name": "Ajin",
+  //       "avatar": "https://it4788.catan.io.vn/files/avatar-1702563442430-19905748.jpg"
+  //     },
+  //     "comments": [
+  //       {
+  //         "content": "so good",
+  //         "created": "2023-12-21T03:20:41.668Z",
+  //         "poster": {
+  //           "id": "1",
+  //           "name": "username",
+  //           "avatar": "https://it4788.catan.io.vn/files/avatar-1703485156307-847663699.jpg"
+  //         }
+  //       },
+  //       {
+  //         "content": "so good",
+  //         "created": "2023-12-21T03:24:19.000Z",
+  //         "poster": {
+  //           "id": "1",
+  //           "name": "username",
+  //           "avatar": "https://it4788.catan.io.vn/files/avatar-1703485156307-847663699.jpg"
+  //         }
+  //       }
+  //     ]
+  //   },
+  //   // ...Thêm các comment khác vào đây
+  // ];
+
+  async function onEndReadable() {
+    if (isNextFetch) {
+      try {
+        setIsNext(true);
+        setSkip(skip => skip + COUNT_ITEM);
+        const res = await getMarkCommentApi({
+          id: id,
+          index: skip,
+          count: COUNT_ITEM
+        });
+        if (res.success) {
+          if (!res.data.length) {
+            setIsNextFetch(false);
+            return;
+          }
+          console.log(skip, res.data);
+          const response = res.data;
+          if (res.data.length) {
+            //console.log('searchResult', searchResult);
+            setListMarkComment(listMarkComments => [...listMarkComments, ...response]);
+          }
+        }
+      } catch (e) {
+        setSkip(skip => skip - COUNT_ITEM);
+        return;
+      } finally {
+        setIsNext(false);
+      }
+    }
+  }
   // const navigateEditProfileScreen = () => navigation.navigate('EditProfile');
   const handleCancel = () => {
-    props.setOpenModal(false);
+    setOpenModal(false);
   };
+  // console.log('id', id)
   return (
     <ScrollView>
       <Modal
-        isVisible={props.openModal}
+        isVisible={openModal}
         style={styles.container}
         animationIn='slideInUp'
         animationOut='slideOutDown'
         backdropOpacity={0.5}
         onBackdropPress={handleCancel}
       >
-        <View style={styles.header}>
-          {/* <Text>đây là header comment</Text> */}
-          <View style={styles.icon}>
-            {/* <AntdIcon name='like1' size={18} color='#02ADFC' style={styles.like} />
-          <Ionicons name='heart-circle' size={18} color='#FF74AE' style={styles.heart} />
-          <FontAwesomeIcon6 name='laugh-squint' size={18} color='#FFF298' style={styles.laugh} />
-          <Text style={styles.numbericon}> 1000 </Text>
-          <AntdIcon name='right' size={18} color='#000' style={styles.like} /> */}
-            <PostReactions emotions={Emotions} />
-          </View>
-        </View>
-        <View>
-          <ListComments listComments={listComments} />
-        </View>
-        <View>
-          <Text>đây là header comment</Text>
-        </View>
+        {isLoadingFirstApi ? (
+          <ActivityIndicator color={color.activeOutlineColor} style={{ marginTop: '50%' }} />
+        ) : (
+          <>
+            <TouchableHighlight style={styles.header}>
+              {/* <Text>đây là header comment</Text> */}
+              <View style={styles.icon}>
+                <View style={styles.left}>
+                  <View style={styles.listIcon}>
+                    <AntdIcon
+                      name='like1'
+                      size={18}
+                      color={color.likeIcon}
+                      style={styles.rightIcon}
+                    />
+                    <AntdIcon
+                      name='dislike1'
+                      size={18}
+                      color={color.iconButtonColor}
+                      style={styles.rightIcon}
+                    />
+                    {listFeels.length && (
+                      <Text style={{ color: color.iconButtonColor }}>
+                        {listFeels[0].feel.user.name} và {totalFeel - 1} người khác
+                      </Text>
+                    )}
+                    <AntdIcon
+                      name='right'
+                      size={18}
+                      color={color.iconButtonColor}
+                      style={styles.rightIcon}
+                    />
+                  </View>
+                </View>
+              </View>
+            </TouchableHighlight>
+            <View>
+              <BaseFlatList
+                ListHeaderComponent={() => (
+                  <TouchableHighlight style={{ padding: 10 }} underlayColor={color.Comment}>
+                    <Text
+                      // style={{ fontSize: 14, fontWeight: '700', marginBottom: 4 }}
+                      style={styles.viewBeForeComment}
+                    >
+                      Tất cả bình luận
+                    </Text>
+                  </TouchableHighlight>
+                )}
+                data={listMarkComment}
+                renderItem={({ item }) => (
+                  <>
+                    <View>
+                      <View style={styles.CommentItem}>
+                        <View style={styles.top}>
+                          <TouchableHighlight
+                            // activeOpacity={0.8}
+                            // underlayColor={color.borderColor}
+                            onPress={() => {}}
+                            style={styles.touchableHighlight}
+                            underlayColor={color.Comment}
+                          >
+                            <View>
+                              <Avatar.Image
+                                source={getAvatarUri(item.poster.avatar)}
+                                size={40}
+                                style={styles.avatarImage}
+                              />
+                            </View>
+                          </TouchableHighlight>
+                          <View style={styles.Content}>
+                            <Text style={styles.TextName}>{item.poster.name}</Text>
+                            <Text style={styles.TextCommment}>{item.mark_content}</Text>
+                          </View>
+                        </View>
+                        <View style={styles.Function}>
+                          <Text style={styles.TextTime}>{coverTimeToNow(item.created)}</Text>
+                          <TouchableHighlight
+                            onPress={() => {}}
+                            style={styles.highlightText}
+                            underlayColor={color.Comment}
+                          >
+                            <Text style={styles.TextLike}> Thích</Text>
+                          </TouchableHighlight>
+                          <TouchableHighlight
+                            onPress={() => {}}
+                            style={styles.highlightConment}
+                            underlayColor={color.Comment}
+                          >
+                            <Text style={styles.TextResponse}> Phản hồi</Text>
+                          </TouchableHighlight>
+                        </View>
+                        {item.comments.length > 0
+                          ? item.comments.map((repItem: any) => (
+                              <>
+                                <View style={styles.topRep}>
+                                  <TouchableHighlight
+                                    onPress={() => {}}
+                                    style={styles.touchableHighlight}
+                                    underlayColor={color.Comment}
+                                  >
+                                    <View>
+                                      <Avatar.Image
+                                        source={getAvatarUri(repItem.poster.avatar)}
+                                        size={25}
+                                        style={styles.avatarImage}
+                                      />
+                                    </View>
+                                  </TouchableHighlight>
+                                  <View style={styles.ContentRep}>
+                                    <Text style={styles.TextNameRep}>{repItem.poster.name}</Text>
+                                    <Text style={styles.TextCommmentRep}>{repItem.content}</Text>
+                                  </View>
+                                </View>
+                                <View style={styles.FunctionRep}>
+                                  <Text style={styles.TextTime}>
+                                    {coverTimeToNow(repItem.created)}
+                                  </Text>
+                                  <TouchableHighlight
+                                    onPress={() => {}}
+                                    style={styles.highlightText}
+                                    underlayColor={color.Comment}
+                                  >
+                                    <Text style={styles.TextLike}> Thích</Text>
+                                  </TouchableHighlight>
+                                  <TouchableHighlight
+                                    onPress={() => {}}
+                                    style={styles.highlightConment}
+                                    underlayColor={color.Comment}
+                                  >
+                                    <Text style={styles.TextResponse}> Phản hồi</Text>
+                                  </TouchableHighlight>
+                                </View>
+                              </>
+                            ))
+                          : null}
+                      </View>
+                    </View>
+                  </>
+                )}
+                keyExtractor={item => item.id}
+                isFootterLoading={isNext}
+                onEndReached={onEndReadable}
+                onEndReachedThreshold={0.001}
+              />
+            </View>
+          </>
+        )}
       </Modal>
     </ScrollView>
   );
--- src/screens/search/SearchScreen/SearchScreen.tsx
+++ src/screens/search/SearchScreen/SearchScreen.tsx
@@ -1,7 +1,6 @@
 import { View, Text, TouchableOpacity, TextInput, Keyboard } from 'react-native';
 import { useEffect, useState } from 'react';
 import styles from './styles';
-import CommentTab from '../../../components/Post/components/Comment/CommentModal';
 import { ActivityIndicator, Appbar, Divider, IconButton } from 'react-native-paper';
 import { color } from 'src/common/constants/color';
 import HistorySearch from './compoment/HistorySearch';
@@ -38,13 +37,12 @@ function SearchScreen() {
     USER: 'Mọi người'
   };
   const COUNT_ITEM = 10;
-  const [openModal, setOpenModal] = useState(false);
   const [openModalHistorySearch, setOpenModalHistorySearch] = useState(false);
   const [searchText, setSearchText] = useState('');
   const [isRefresh, setIsRefresh] = useState(false);
   const [isRefreshSaveSearch, setIsRefreshSaveSearch] = useState(false);
   const [isSearching, setIsSearching] = useState(false);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(90);
   const [listSearch, setListSearch] = useState<ISearchResult[]>([]);
   const [isConnected, setIsConnected] = useState<boolean | null>(true);
   const [isLoadingFirstApi, setIsLoadingFirstAPi] = useState<boolean>(false);
@@ -121,17 +119,18 @@ function SearchScreen() {
         }
         setData(res.data);
         setIsNextFetch(true);
-        setSkipPost(COUNT_ITEM);
+        setSkip(COUNT_ITEM);
       }
       setTimeout(() => {
         setIsLoadingFirstAPi(false);
-      }, 200);
+      }, 300);
 
       // setCurrentIndex(0)
     } catch (error) {
       return console.log({ message: 'sever availability' });
     }
   };
+
   async function onEndReadable() {
     if (searchText !== '' && isNextFetch) {
       try {
@@ -245,7 +244,7 @@ function SearchScreen() {
   useEffect(() => {
     const data: IGetSavedSearch = {
       index: 0,
-      count: 100
+      count: 10
     };
     const fetchData = async (data: IGetSavedSearch) => {
       try {
@@ -349,7 +348,7 @@ function SearchScreen() {
                 setIsLoadingFirstAPi(true);
                 setTimeout(() => {
                   setIsLoadingFirstAPi(false);
-                }, 200);
+                }, 300);
               }}
             >
               Bài viết
@@ -361,7 +360,7 @@ function SearchScreen() {
                 setIsLoadingFirstAPi(true);
                 setTimeout(() => {
                   setIsLoadingFirstAPi(false);
-                }, 200);
+                }, 300);
               }}
             >
               Mọi người
@@ -507,7 +506,6 @@ function SearchScreen() {
             )}
           </View>
         </View>
-        <CommentTab openModal={openModal} setOpenModal={setOpenModal} />
         <HistorySearch
           openModal={openModalHistorySearch}
           setOpenModal={setOpenModalHistorySearch}
