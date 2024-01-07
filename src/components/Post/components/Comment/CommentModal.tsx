c7de4f1a8ca941a70ee1ba17d8c33ce81647aad1
NHT18102002 <truyen18102002@gmail.com>
2024-01-08 00:34:10 +0700

    feat: update-rep-comment

--- src/components/Post/components/Comment/CommentModal.tsx
+++ src/components/Post/components/Comment/CommentModal.tsx
@@ -5,7 +5,7 @@ import { color } from 'src/common/constants/color';
 import BaseFlatList from 'src/components/BaseFlatList';
 import { ActivityIndicator, Appbar, Avatar, Divider } from 'react-native-paper';
 import { getAvatarUri } from 'src/utils/helper';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { IListFeels } from 'src/interfaces/comments.interface';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { AppNaviagtionName, ProfileNavigationName } from 'src/common/constants/nameScreen';
@@ -17,6 +17,8 @@ import {
 import { IListCommentPost } from 'src/interfaces/comments.interface';
 import { coverTimeToNow } from 'src/utils/dayjs';
 import AntdIcon from 'react-native-vector-icons/AntDesign';
+import { useAppDispatch } from 'src/redux';
+import { changeCoins } from 'src/redux/slices/authSlice';
 interface CommentTabProps {
   id: string;
   openModal: boolean;
@@ -24,12 +26,15 @@ interface CommentTabProps {
   listMarkComment: IListCommentPost[];
   setListMarkComment: React.Dispatch<React.SetStateAction<IListCommentPost[]>>;
   numberFeel: number;
+  typeMark: any;
 }
 const CommentTab = (props: CommentTabProps) => {
   const navigationProfile: NavigationProp<AppNavigationType, AppNaviagtionName.ProfileNavigation> =
     useNavigation();
-  const { id, openModal, setOpenModal, setListMarkComment, listMarkComment, numberFeel } = props;
+  const { id, openModal, setOpenModal, setListMarkComment, listMarkComment, numberFeel, typeMark } =
+    props;
   // const [listMarkComment, setListMarkComment] = useState<IListCommentPost[]>([]);
+  const dispatch = useAppDispatch();
   const [skip, setSkip] = useState<number>(0);
   const [isNext, setIsNext] = useState<boolean>(false);
   const [isNextFetch, setIsNextFetch] = useState<boolean>(true);
@@ -37,10 +42,12 @@ const CommentTab = (props: CommentTabProps) => {
   const [listFeels, setListFeels] = useState<IListFeels[]>([]);
   const [totalFeel, setTotalFeel] = useState(0);
   const [kudosFeel, setKudosFeel] = useState(0);
+  const [markId, setMarkId] = useState(0);
   const [commentText, setCommentText] = useState('');
   const [openModalListFeels, setOpenModalListFeel] = useState(false);
+  const [isComment, setIsComment] = useState(false);
   const COUNT_ITEM = 10;
-
+  const textInputRef = useRef<TextInput>(null);
   const handleNavigationProfile = (id: string) =>
     navigationProfile.navigate(AppNaviagtionName.ProfileNavigation, {
       screen: ProfileNavigationName.Profile,
@@ -52,7 +59,7 @@ const CommentTab = (props: CommentTabProps) => {
     setSkip(COUNT_ITEM);
     setTimeout(() => {
       setIsLoadingFirstAPi(false);
-    }, 1000);
+    }, 1500);
   }, []);
 
   const handleListFeel = async () => {
@@ -122,14 +129,15 @@ const CommentTab = (props: CommentTabProps) => {
         content: commentText,
         index: 0,
         count: 10,
-        mark_id: null,
-        type: 0
+        mark_id: markId == 0 ? null : markId,
+        type: typeMark
       });
       if (result.success) {
         if (!result.data.length) {
           return;
         }
         setListMarkComment(result.data);
+        dispatch(changeCoins(result.data.coins));
         // const response = res.data;
       }
     } catch (e) {
@@ -137,13 +145,19 @@ const CommentTab = (props: CommentTabProps) => {
     }
     setCommentText('');
     setSkip(0);
+    setMarkId(0);
   };
 
   const handleTextChange = (e: any) => {
     setCommentText(e);
   };
+  const handleResponse = (id_comment: string) => {
+    if (textInputRef.current) {
+      textInputRef.current?.focus();
+      setMarkId(parseInt(id_comment, 10));
+    }
+  };
   // console.log('id', id)
-
   // const ModalComment = ({ visible, onClose, children }: { visible: boolean, onClose: () => void, children: React.ReactNode }) => {
   //   const translateY = useRef(new Animated.Value(0)).current;
   //   const panResponder = useRef(
@@ -244,7 +258,7 @@ const CommentTab = (props: CommentTabProps) => {
               </View>
             </View>
           </TouchableHighlight>
-          <View style={{ height: '86%' }}>
+          <View style={isComment ? { height: '75%' } : { height: '86%' }}>
             <BaseFlatList
               ListHeaderComponent={() => (
                 <TouchableHighlight style={{ padding: 10 }} underlayColor={color.Comment}>
@@ -301,13 +315,21 @@ const CommentTab = (props: CommentTabProps) => {
                           style={styles.highlightConment}
                           underlayColor={color.Comment}
                         >
-                          <Text style={styles.TextResponse}> Phản hồi</Text>
+                          <Text
+                            style={styles.TextResponse}
+                            onPress={() => {
+                              handleResponse(item.id);
+                            }}
+                          >
+                            {' '}
+                            Phản hồi
+                          </Text>
                         </TouchableHighlight>
                       </View>
                       {item.comments.length > 0
                         ? item.comments.map((repItem: any, index: number) => (
                             <>
-                              <View key={index} style={styles.topRep}>
+                              <View key={index.toString() + repItem.content} style={styles.topRep}>
                                 <TouchableHighlight
                                   onPress={() => {}}
                                   style={styles.touchableHighlight}
@@ -361,6 +383,9 @@ const CommentTab = (props: CommentTabProps) => {
           <Divider />
           <View>
             <TextInput
+              ref={textInputRef}
+              onFocus={() => setIsComment(true)}
+              onBlur={() => setIsComment(false)}
               placeholder='Viết bình luận'
               clearButtonMode='always'
               onSubmitEditing={handlePressEnter}
